c1ebce1856513a3eddfe248cca4a544236e56f3d
Bhaumik Vasav Bhatt <Bhaumik.Vasav.Bhatt@hgst.com>
2016-05-20 17:43:04 -0600

    Added rocksdb to list of dependencies, feature suggestions

--- kaos/tools/install-packages.sh
+++ kaos/tools/install-packages.sh
@@ -1,4 +1,8 @@
 #! /bin/bash
+
+#TODO: Make this file distribution agnostic
+#TODO: Error handling in case libgflags(or any package) is not installed
+
 sudo apt-get update
 sudo apt-get -y install make
 sudo apt-get --yes --force-yes install g++
@@ -7,3 +11,9 @@ sudo apt-get --yes --force-yes install git
 sudo apt-get --yes --force-yes install yagiuda
 sudo apt-get --yes --force-yes install cppcheck
 sudo apt-get --yes --force-yes install doxygen
+
+# glags - a library that handles command line flags processing. You can compile rocksdb library even if you don't have gflags installed.
+sudo apt-get --yes --force-yes install libgflags-dev
+sudo apt-get --yes --force-yes install libsnappy-dev
+sudo apt-get --yes --force-yes install zlib1g-dev
+sudo apt-get --yes --force-yes install libbz2-dev
